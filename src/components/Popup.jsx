e4655f7864bf2445b9e9701acb3c3bf7e2aea5e8
brutumfulmen97 <109813663+brutumfulmen97@users.noreply.github.com>
2023-10-23 12:50:01 +0200

    popup sredjen

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,32 +1,39 @@
 import styles from "../styles/popup.module.scss";
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import Button from "./Button";
 
 export default function Popup() {
-    function wait(ms) {
-        return new Promise((resolve) => setTimeout(resolve, ms));
+    function showPopup() {
+        setTimeout(() => {
+            document.body.style.overflow = "hidden";
+            const popup = document.getElementById("overlay");
+            const popupWidth = popup.offsetWidth;
+            const popupHeight = popup.offsetHeight;
+            const screenWidth = window.innerWidth;
+            const screenHeight = window.innerHeight;
+            const left = (screenWidth - popupWidth) / 2;
+            const top = (screenHeight - popupHeight) / 2;
+            popup.style.left = left + "px";
+            popup.style.top = top + "px";
+            popup.style.display = "block";
+        }, 3000);
+        window.removeEventListener("scroll", debouncedShowPopup);
     }
 
-    const popupCB = useCallback(async () => {
-        await wait(3000);
-        document.body.style.overflow = "hidden";
-        const popup = document.getElementById("overlay");
-        const popupWidth = popup.offsetWidth;
-        const popupHeight = popup.offsetHeight;
-        const screenWidth = window.innerWidth;
-        const screenHeight = window.innerHeight;
-        const left = (screenWidth - popupWidth) / 2;
-        const top = (screenHeight - popupHeight) / 2;
-        popup.style.left = left + "px";
-        popup.style.top = top + "px";
-        popup.style.display = "block";
-        window.removeEventListener("scroll", popupCB);
-    }, []);
+    let debounceTimer;
+    const debounce = (callback, time) => {
+        window.clearTimeout(debounceTimer);
+        debounceTimer = window.setTimeout(callback, time);
+    };
+
+    function debouncedShowPopup() {
+        debounce(showPopup, 500);
+    }
 
     useEffect(() => {
-        window.addEventListener("scroll", popupCB);
-        return () => window.removeEventListener("scroll", popupCB);
-    }, [popupCB]);
+        window.addEventListener("scroll", debouncedShowPopup);
+        return () => window.removeEventListener("scroll", debouncedShowPopup);
+    });
 
     return (
         <div className={styles.overlay} id="overlay">
